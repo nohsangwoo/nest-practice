bd844dde5e9bb12daeec501c7070ebaa9d87f64c
nohsangwoo <nsgr12@naver.com>
2020-10-10 22:34:34 +0900

    empty and start anew

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // controllers url을 호출하고 함수를 실행함
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
