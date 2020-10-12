559165827fa09da1c2ba4ed8c04ef8643871f33b
nohsangwoo <nsgr12@naver.com>
2020-10-12 17:46:10 +0900

    e2e testing for /movies   get and post and delete

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,36 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  // movies에서 get testing
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+    });
+
+    // 서버에 request해서 movies에 post할때 이정보를 보내면 201을 받는지 테스트
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+        .delete('/movies')
+        .expect(404);
+      // 404 error message that means not found
+      /*405 error message that means method not allowed http*/
+    });
   });
 });
