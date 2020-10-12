4de20816925b462ea4ad5df949b7378498a0581c
nohsangwoo <nsgr12@naver.com>
2020-10-13 01:20:50 +0900

    finish to e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,20 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    // 변수의 자동 형변환을 설정해줌 main.ts에서 ValidationPipe설정 해준것과 똑같이 설정해줌
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
     await app.init();
   });
 
+  // 순서대로 테스팅이 진행됨
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
@@ -32,7 +43,7 @@ describe('AppController (e2e)', () => {
     });
 
     // 서버에 request해서 movies에 post할때 이정보를 보내면 201을 받는지 테스트
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -43,12 +54,51 @@ describe('AppController (e2e)', () => {
         .expect(201);
     });
 
+    // 잘못된 obj를 전달했을때 제대로 필터링 되는지 검사
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+
     it('DELETE', () => {
       return request(app.getHttpServer())
         .delete('/movies')
         .expect(404);
       // 404 error message that means not found
       /*405 error message that means method not allowed http*/
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404);
+    });
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200);
     });
   });
 });
